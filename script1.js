5702814850bd6c8eb57c8b8aeae3d3b6f0097626
Job Maina <jobisaacmaina22@gmail.com>
2024-10-01 19:47:52 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,15 +1,48 @@
-document.getElementById('btn').addEventListener('click', function() {
-   
-    var name = document.getElementById('name').value;
-    var message = document.getElementById('message').value;
-    var email= document.getElementById('email').value;
+ 
+ const menuToggle = document.querySelector('.menu-toggle');
+ const menu = document.querySelector('.menu');
+ const themeToggle = document.querySelector('.theme-toggle');
+ const body = document.body;
+ const menuItems = document.querySelectorAll('.menu li');
 
-  
-    var mailtoLink = 'mailto:jobisaacmaina22@gmail.com?subject=Subject&body=Name: ' + encodeURIComponent(name) +
-                     '%0D%0A%0D%0AMessage: ' + encodeURIComponent(message) +
-                     '%0D%0A%0D%0AEmail: ' + encodeURIComponent(email);
 
-   
-    window.location.href = mailtoLink;
-});
+ function showMenu() {
+     menuItems.forEach((item, index) => {
+         gsap.to(item, {
+             opacity: 1,
+             y: 0,
+             stagger: 0.1,
+             duration: 0.5,
+             ease: "power3.out",
+             delay: index * 0.1 
+         });
+         item.classList.add('showItem'); 
+     });
+ }
 
+ function hideMenu() {
+     gsap.to(menuItems, {
+         opacity: 0,
+         y: -20,
+         duration: 0.3,
+         ease: "power3.in",
+         onComplete: function() {
+             menu.classList.remove('show');
+             menuToggle.classList.remove('open');
+         }
+     });
+ }
+
+ menuToggle.addEventListener('click', () => {
+     if (menu.classList.contains('show')) {
+         hideMenu();
+     } else {
+         menu.classList.add('show');
+         menuToggle.classList.add('open');
+         showMenu();
+     }
+ });
+
+ themeToggle.addEventListener('click', () => {
+     body.classList.toggle('dark-mode');
+ });
